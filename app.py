a54f7454a804e80611596408e7804ff3871380f3
Arkaprava Sinha <71584375+arksinha93@users.noreply.github.com>
2021-05-06 22:32:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def index():
-    return "Hello, You're using Python App"
+def hello_world():
+    return "Welcome to Flask Application"
 
-app.run(debug=True, host='0.0.0.0')
+app.run(host='0.0.0.0', port=5000)
